437d67ba1e1abf4fe43dd9a7d7b330b7c189b279
Mindaphil Francisca Sotelo <smindaphil@gmail.com>
2026-09-10 19:55:04 +0800

    Add explicit /dashboard route to App.jsx

--- interntrackr-client/src/App.jsx
+++ interntrackr-client/src/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
           <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
         </Routes>
       </BrowserRouter>
